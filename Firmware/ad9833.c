b442c56e11fb8131cfaf38f3fedda2a84312e249
polymurph <eddyed.k@gmail.com>
2022-09-06 19:57:56 +0200

    working on the ad9833 driver

---
+++ Firmware/ad9833.c
@@ -0,0 +1,21 @@
+typedef enum {
+    _reg_FREQ0 = 0x00,
+    _reg_FREQ1 = 0x00
+}reg_t;
+
+typedef enum {
+    _bit_FSELECT = 0x00,
+    _bit_D1 = 0x00,
+    _bit_MODE = 0x0002,
+    _bit_DIV2 = 0x0008,
+    _bit_OPBITEN = 0x0020,
+    _bit_SLEEP12 = 0x0040,
+    _bit_SLEEP1 = 0x0080,
+    _bit_RESET = 0x0100,
+    _bit_PSELECT = 0x0400,
+    _bit_FSELECT = 0x0800,
+    _bit_HLB = 0x1000,
+    _bit_B28 = 0x2000
+}bit_t;
+
+
--- Firmware/ad9833.h
+++ Firmware/ad9833.h
@@ -1,9 +1,18 @@
 #ifndef _AD9833_H_
 #define _AD9833_H_
 
+#include <stdint.h>
+#include "spi.h"
+
 typedef struct {
 
 } ad9833_t;
+
+void ad9833_init(ad9833_t *device);
+
+void ad9833_setMainClockSleepMode(ad9833_t *device, uint8_t logic);
+
+void ad9833_
 
 
 #endif // _AD9833_H_
